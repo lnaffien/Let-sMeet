75e5f439ddf33c0da203a68a0e7c19cbd3f271d3
unknown <lucie@naffien.com>
2022-03-03 13:33:46 +0100

    Toolbar : search action

--- Let-sMeet/app/src/main/java/com/example/letsmeet/MainActivity.kt
+++ Let-sMeet/app/src/main/java/com/example/letsmeet/MainActivity.kt
@@ -1,42 +1,138 @@
 package com.example.letsmeet
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.letsmeet.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import java.util.*
 
 class MainActivity : AppCompatActivity()
 {
 
-    private lateinit var binding: ActivityMainBinding
+//    private lateinit var binding: ActivityMainBinding
+
     private lateinit var drawer: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(binding.toolbar)
+        setSupportActionBar(toolbar)
+
+        val actionBar = supportActionBar
+        actionBar?.show()
 
         drawer = findViewById(R.id.drawer_layout)
+//
+//        toggle = ActionBarDrawerToggle(this, drawer, 0, 0)
+//        drawer.addDrawerListener(toggle)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        supportActionBar?.setHomeButtonEnabled(true)
+//
+//        toggle.syncState()
+//
+        navView = findViewById(R.id.drawer_nav_view)
+        setNavDrawerEvent()
+    }
 
-/*        toggle = ActionBarDrawerToggle(this, drawer, "Ouvert", "Ferme")
-        drawer.addDrawerListener(toggle)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setHomeButtonEnabled(true)*/
+    /**
+     * When a slide from left to right happened
+     */
+//    override fun onSupportNavigateUp(): Boolean {
+//        drawer.openDrawer(navView)
+//        return true
+//    }
+
+    /********************************
+     *          DRAWER              *
+     ********************************/
+
+    /**
+     * When the phone's back button is pressed
+     */
+    override fun onBackPressed() {
+        if(this.drawer.isDrawerOpen(GravityCompat.START))
+        {
+            this.drawer.closeDrawer(GravityCompat.START)
+        }
+        else
+        {
+            super.onBackPressed()
+        }
+    }
+
+    private fun setNavDrawerEvent()
+    {
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId)
+            {
+                R.id.drawer_flux ->
+                {
+                    Toast.makeText(this, R.string.flux, Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.drawer_my_events ->
+                {
+                    Toast.makeText(this, R.string.my_events, Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.drawer_contact ->
+                {
+                    Toast.makeText(this, R.string.contacts, Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.drawer_settings ->
+                {
+                    Toast.makeText(this, R.string.settings, Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.drawer_disconnection ->
+                {
+                    Toast.makeText(this, R.string.disconnection, Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else ->
+                {
+                    false
+                }
+            }
+        }
+    }
+
+    /********************************
+     *          TOOLBAR             *
+     ********************************/
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean
+    {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+//        searchItem = menu?.findItem(R.id.toolbar_search)
+//        searchView=searchItem?.actionView
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.toolbar_search ->
+            {
+                Toast.makeText(this, R.string.toolbar_search, Toast.LENGTH_SHORT).show()
+                true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
